a76eb2168a3f0e6e8b40a7b2ae8c682c3512948a
A. V <anysiavalko@gmail.com>
2018-11-06 03:07:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
+  else
+    return false
+  end
+end
+#The method takes a number and divides it by all the numbers that come before it to see if there is a remainder. All the results have to show a remainder for it to be prime. If there is a remainder for all numbers, it will return true.
